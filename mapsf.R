080672a2e1ed6cac3de9fc716d791a16e405a010
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-05-11 08:15:37 +0300

    Commit of choropleth map

--- mapsf.R
+++ mapsf.R
@@ -50,6 +50,37 @@ mf_layout(title = 'Population Distribution in Martinique',
                            'mapsf ',
                            packageVersion('mapsf')))
 
+# Choropleth map
+library(mapsf)
+
+# Import the sample data set
+mtq <- mf_get_mtq()
+
+# Population density (inhab./km2) using sf::st_area()
+mtq$POPDENS <- 1e6 * mtq$POP / st_area(mtq)
+
+# Set a theme
+mf_theme("green")
+
+# Plot population density
+mf_map(
+  x = mtq, 
+  var = "POPDENS",
+  type = "choro",
+  breaks = "geom",
+  nbreaks = 5,
+  col = "Mint",
+  border = "white", 
+  lwd = 0.5,
+  leg_pos = "topright", 
+  leg_title = "Population Density\n(people per km2)"
+) 
+
+# Layout
+mf_layout(title = "Population Distribution in Martinique", 
+          credits = paste0("Sources: Insee and IGN, 2018\n",
+                           "mapsf ", 
+                           packageVersion("mapsf")))
 
 
 

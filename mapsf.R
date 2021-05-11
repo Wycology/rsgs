9ea6245b49dc8fb9821777568705e10b5565261e
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-05-11 08:11:28 +0300

    Awesome proportional map of population at Martinique

--- mapsf.R
+++ mapsf.R
@@ -19,12 +19,36 @@ mf_layout(title = 'Martinique',
           packageVersion('mapsf')))
 
 # Proportional symbols
+library(mapsf)
 
+# Import the sample data set
+mtq <- mf_get_mtq()
 
+# Initiate base map
+mf_init(x = mtq, theme = 'iceberg')
 
+# Plot a shadow
+mf_shadow(mtq, add = TRUE)
 
+# Plot municipalities
+mf_map(mtq, add = TRUE)
 
+# Plot population
+mf_map(
+  x = mtq,
+  var = 'POP',
+  type = 'prop',
+  inches = 0.25,
+  col = 'purple',
+  leg_pos = 'bottomleft2',
+  leg_title = 'Total population'
+)
 
+# Layout
+mf_layout(title = 'Population Distribution in Martinique',
+          credits = paste0('Sources: Insee and IGN, 2018\n',
+                           'mapsf ',
+                           packageVersion('mapsf')))
 
 
 

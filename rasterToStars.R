7b5bebfb4c5ac581bb88e82c5b2b4326a38593ac
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-05-21 09:48:49 +0300

    Loading the libs

--- rasterToStars.R
+++ rasterToStars.R
@@ -1,4 +1,8 @@
 # In this code, I am going to demonstrate how to convert raster to stars object
 # Loading the libraries
 
+library(raster)
+library(stars)
+library(sf)
+
 # First is to create a raster object

db198b3b60c1564f244a0eec2ca8ef09fc62e2d9
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-06-01 12:49:20 +0300

    getting polygon data

--- esri2sf.R
+++ esri2sf.R
@@ -16,9 +16,9 @@ plot(df)
 
 url <- 'https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Florida_Annual_Average_Daily_Traffic/FeatureServer/0'
 df <- esri2sf(url, outFields = c('AADT', 'DFLG'))
-
 plot(df)
 
+# Sourcing polygon data from url
 url <- 'https://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Demographics/ESRI_Census_USA/MapServer/3'
 df <- esri2sf(url,
               where = "STATE_NAME = 'Michigan'",

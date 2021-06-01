64ae3e630152ea3e047528a97f2d55cd80bf7d68
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-06-01 12:39:17 +0300

    Extracting data from ArcGis Server

---
+++ esri2sf.R
@@ -0,0 +1,21 @@
+# Getting shapefile data from ARCGIS Server
+library(remotes)
+install_github('yonghah/esri2sf')
+
+library(esri2sf)
+
+url <- 'https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Landscape_Trees/FeatureServer/0'
+df <- esri2sf(url)
+plot(df)
+
+url <- 'https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Florida_Annual_Average_Daily_Traffic/FeatureServer/0'
+df <- esri2sf(url, outFields = c('AADT', 'DFLG'))
+
+plot(df)
+
+url <- 'https://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Demographics/ESRI_Census_USA/MapServer/3'
+df <- esri2sf(url,
+              where = "STATE_NAME = 'Michigan'",
+              outFields = c('POP2000', 'pop2007', 'POP00_SQMI', 'POP07_SQMI'))
+
+plot(df)

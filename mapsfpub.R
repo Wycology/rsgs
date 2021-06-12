18caaf8a675285bf19c63cc70e7711227b40aa14
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-06-12 09:51:13 +0300

    Awesome plot

--- mapsfpub.R
+++ mapsfpub.R
@@ -5,8 +5,9 @@ study_area <- mf_get_mtq() # Loading the sample data
 mf_init(study_area, expandBB = rep(0, 4), theme = 'candy') # Initating the map  
 
 mf_shadow(study_area, col = 'purple', cex = 2, add = TRUE) # Adding the shadow
-mf_map(study_area, add = TRUE)
-mf_map(x = study_area, var = c("POP", "MED"), type = "prop_choro", border = "grey50",
+mf_map(study_area, add = TRUE, border = 'maroon', lwd = 0.5, 
+       leg_pos = c(720000, 1628119))
+mf_map(x = study_area, var = c("POP", "MED"), type = "prop_choro", border = "black",
        lwd = 1, leg_pos = c("topright", "right"), leg_title = c("Population","Median\nIncome\n(in euros)"), 
        breaks = "equal", nbreaks = 4, pal = "Reds", leg_val_rnd = c(0, -2), 
        leg_frame = c(TRUE, TRUE), leg_val_cex = c(1, 1.2)

d912ff3479fdb8b86f024b0ee01ff187ce0483e5
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-11-17 20:29:07 +0300

    c

--- esri2sf.R
+++ esri2sf.R
@@ -1,7 +1,7 @@
 # Getting shapefile data from ARCGIS Server
 # Created by Wyclife Agumba Oluoch
 
-library(remotes) # For use in installing the package from git
+library(remotes) # For use in installing the package from g
 install_github('yonghah/esri2sf') # Downloading the package
 
 library(esri2sf) # Loading the installed library

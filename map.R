0760a741ce362db0680f4ad6ca49b8d00bd6870d
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-11-20 23:22:46 +0300

    c

--- map.R
+++ map.R
@@ -1,4 +1,4 @@
-library(raster) # For getting shapefile and raster data onlin
+library(raster) # For getting shapefile and raster data on
 library(mapsf)  # For plotting the simple feature maps
 library(sf)     # For gettinf functions for transforming coordinates
 library(tidyverse) # Data wrangling package

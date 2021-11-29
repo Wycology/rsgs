fcb99f75f706e01b04e596cd837d1c0232d0e78c
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-11-29 18:24:22 +0300

    c

--- mapsf.R
+++ mapsf.R
@@ -5,7 +5,7 @@
 
 library(mapsf) # for mapping sf objects
 
-mtq <- mf_get_mtq() # Loading 
+mtq <- mf_get_mtq() # Loadin
 
 mf_init(x = mtq, expandBB = rep(0, 4), theme = 'jsk') # Initializing the mapping process
 

977ab47e07132151e7334d6bb96d69163e2248a7
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-12-26 20:50:41 +0300

    c

--- mapsfpub.R
+++ mapsfpub.R
@@ -21,7 +21,7 @@ mf_scale(cex = 1.5)
 mf_credits(paste0('Sources: IGN, 2018, Package: ', 'mapsf ', 'Version: ', packageVersion('mapsf')), 
            cex = 1.5, bg = 'magenta')
 mf_title('Study Area: Martinique', cex = 2) 
-mf_arrow() # Adding the north arrow
+mf_arrow() # Adding the north arr
 mf_inset_on(x = 'worldmap', pos = 'left')
 mf_worldmap(study_area, col = "red") 
 mf_inset_off() 

9b99b827be5be175267699298364f7abc6ecf42f
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2022-01-01 19:48:31 +0300

    c

--- map.R
+++ map.R
@@ -3,7 +3,7 @@ library(mapsf)
 library(sf)     
 library(tidyverse) 
 
-kenya <- getData('GADM', country = 'KEN', level = 1) # Getting the Kenyan data
+kenya <- getData('GADM', country = 'KEN', level = 1) # Getting the Kenyan da
 kenya_sf <- st_as_sf(kenya)
 
 clim <- getData('worldclim', var = 'tmin', res = 10)
